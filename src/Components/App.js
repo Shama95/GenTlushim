47e3b852651269e6c8f1c5ad79b1ce9d43920aa1
Shama <eligil95@gmail.com>
2021-04-12 12:12:23 +0300

    adding shama

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import Shama from './Shama';
 const App=()=>{
     return (
     <div>
         App
+        <Shama></Shama>
     </div>
     );
 }
---
+++ src/Components/Shama.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Shama=()=>{
+    return (
+        <div>
+            Shama
+        </div>
+    );
+}
+export default Shama;
