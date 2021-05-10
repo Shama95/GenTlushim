19906c95424c1500ee51f7dd2a61b7803d44f8a6
Shama <eligil95@gmail.com>
2021-05-10 18:49:17 +0300

    shortened the comments

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,9 +2,9 @@ import React from 'react';
 import Link from './Link'
 
 const Header =()=>{
-    return (//this component changes each url according to what the user presses in the header.
+    return (//changing the url for navigation purposes.
         <div className="ui secondary pointing menu">
-            
+
             <Link href="/Tlush" className="item">
                 Tlush
             </Link>
