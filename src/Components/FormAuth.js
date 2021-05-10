bef573e35cc4604976895eafeff3ad0b3c122e10
Aviv Levi <AvivLevi815@gmail.com>
2021-05-10 13:37:59 +0300

    Add logo and border

--- src/Components/FormAuth.js
+++ src/Components/FormAuth.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import { Container } from 'semantic-ui-react';
+import logo from '../Assets/shahar.jpg'
 
-const FormAuth = () => {
+const FormAuth = props => {
     return (
+        <div>
+                <h2 className="ui right header " style={{textAlign: 'right'}}>
+                    <div className="content">
+                        {props.name}
+                        <div className="sub header">{props.personalNumber}</div>
+                    </div>
+                    <img src={logo} style={{marginLeft:"10px"}}/>
+
+                </h2>
+           
+            <br/>
             <Container>
-                <form class="ui form">
-                    <div class="two fields">
-                        <div class="field">
+                <form className="ui form" style={{borderRadius:"10px" , border:"solid grey", borderSpacing:"100px",
+                                                    borderCollapse: "separate"}}>
+                    <div className="two fields">
+                        <div className="field">
                             <label>שם פרטי</label>
                             <input type="text" name="first-name" placeholder="First Name"/>
                         </div>
 
-                        <div class="field">
+                        <div className="field">
                             <label>שם משפחה</label>
                             <input type="text" name="last-name" placeholder="Last Name"/>
                         </div>
@@ -19,18 +32,22 @@ const FormAuth = () => {
 
                     <div>
                         <label>צוות מיועד</label>
-                        <select class="ui fluid dropdown">
+                        <select className="ui fluid dropdown">
                             <option value="otzma">חוקת עוצמ"ה</option>
                             <option value="keva">חוקת קבע</option>
                             <option value="midgamim">מדגמים</option>
+                            <option value="midgamim">צוות שכר</option>
+
                         </select>
                     </div>
 
-                    <button class="ui button" type="submit">שלח בקשה</button>
-                    <button class="ui button" type="submit">האם המשתמש קיים?</button>
+                    <button className="ui button" type="submit">שלח בקשה</button>
+                    <button className="ui button" type="submit">האם המשתמש קיים?</button>
 
                 </form>
             </Container>
+            </div>
+
         );
 };
 
